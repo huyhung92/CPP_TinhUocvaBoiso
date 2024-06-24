657bab7d8020afa78cdacb76b5688815b360e1cd
SPC <SPC@33-LAB402>
2024-06-24 19:05:21 +0700

    Add project files.

---
+++ CPP_TinhUocvaBoiso.cpp
@@ -0,0 +1,17 @@
+// CPP_TinhUocvaBoiso.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "tinh uoc so";
+    cin >> n;
+    for (int i = 1; i = n; i++) {
+        if (n % i == 0);
+            cout << "uoc la" << i << " " << endl;
+    }
+    return 0;
+}
+
+
